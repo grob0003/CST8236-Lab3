caff1f4bd0d04b8ec5c49221e3b9fc4eb4bc9c36
sindieg <sindie.grobbelaar@gmail.com>
2016-12-03 13:32:39 -0500

    Updated

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -3,61 +3,41 @@ using System.Collections;
 
 public class MoveBall : MonoBehaviour
 {
-    // Direction we will be moving in.
-      public Vector3 direction;
+    public float movementSpeed = 2.0f;
+    Vector3 direction;
 
-    // Whether or not we want to use 'discrete-styled' movement. 
-    public bool isDiscrete = true;
+    public Rigidbody rb;
+    public AudioSource sound;
+    public ParticleSystem pSys;
 
-    // Use this for initialization
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+   
+    }
+
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        direction = new Vector3(hor, 0.0f, ver); 
+        rb.AddForce(direction * movementSpeed); 
 
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void OnCollisionEnter(Collision collision)
     {
-
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        // Only move forward if we're working in discrete mode.
-    //        if (true)
-    //        {
-    //            //transform.Translate(Vector3.forward * Time.deltaTime*300);
-    //            Vector3 fwd = transform.TransformDirection(Vector3.forward);
-
-    //            Physics.Raycast(transform.position, fwd, 10);
-    //        }
-
-    //    }
-    //}
-        if (Input.GetKeyDown(KeyCode.Space))
+    
+        if (collision.gameObject.name != "Floor" && collision.gameObject.name != "Top")
         {
-            // Only move forward if we're working in discrete mode.
-            if (isDiscrete)
-            {
-                transform.localPosition += direction;
-            }
-            else
-            {
-                // Stores any information relevant to a collision.
-                RaycastHit hit;
+           Instantiate(pSys, gameObject.transform.position, Quaternion.identity);
+            sound.Play();
 
-                // This call will only populate the above variable if it returns true.
-                bool didHit = Physics.Raycast(
-                  transform.position, // where we start the ray from
-                  direction, // direction of the ray.
-                  out hit, // variable to store results of any collision
-                  direction.magnitude // length of the ray
-                  );
-
-                if (didHit)
-                {
-                    //transform.position = hit.point;
-                    transform.position += (hit.distance * direction.normalized);
-                }
-            }
         }
+
+
     }
 }
